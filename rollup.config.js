dd1e2a4ba294cf15c0d2c6872869cbee2f767eae
Celine Souchet <celine.souchet@bonitasoft.com>
2021-02-05 18:14:05 +0100

    Generate ES6 and CommonJs bundles + Add serve plugins for Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,11 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import builtins from "rollup-plugin-node-builtins";
 import json from "@rollup/plugin-json";
 import sizes from "rollup-plugin-sizes";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import sourceMaps from "rollup-plugin-sourcemaps";
+
+import * as tscompile from "typescript";
 
 import pkg from "./package.json";
 
@@ -12,19 +17,33 @@ export default [
     input: "src/index.ts",
     output: [
       {
-        file: `dist/index.es.js`,
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
         format: "es",
         sourcemap: true
       }
     ],
     external: [...Object.keys(pkg.peerDependencies || {})],
     plugins: [
-      typescript(),
+      typescript({ typescript: tscompile, clean: true }),
       nodeResolve({ mainFields: ["module"], modulesOnly: true }),
       commonjs(),
       builtins(),
       sizes(),
-      json()
+      json(),
+      serve({
+        contentBase: ["dist", "static"],
+        port: 8080,
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      }),
+      livereload({ watch: "dist", verbose: true }),
+      sourceMaps()
     ]
   }
 ];
